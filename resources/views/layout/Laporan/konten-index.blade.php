618f5cc8bc6570614b69ef96aecd487db2d11951
unknown <jejakcodingws@gmail.com>
2024-06-13 11:23:10 +0700

    revisi laporan

--- resources/views/layout/Laporan/konten-index.blade.php
+++ resources/views/layout/Laporan/konten-index.blade.php
@@ -6,7 +6,7 @@
     <div class="content-header">
       <div class="card shadow mb-4">
         @include('layout/flash-message')
-                                <a href="{{route('site-karyawan')}}" >
+                                <a href="#" >
                                 <div class="card-header py-3">
                                     <h6 class="m-0 font-weight-bold text-primary">
                                     <span><i class="fa-solid fa-book-bookmark fa-bounce"></i></span>    
@@ -16,61 +16,60 @@
 
                                 <div class="card-body">
                                 <div class="row">
-                                    <div class="col-lg-3 col-6">
 
+                                    <div class="col-lg-3 col-6">
                                     <div class="small-box bg-info">
-                                    <div class="inner">
-                                    <h3>#</h3>
-                                    <p>Laporan Pendapatan</p>
-                                    </div>
                                     <div class="icon">
                                     <i class="ion ion-bag"></i>
                                     </div>
-                                    <a href="#" class="small-box-footer">cek data <i class="fas fa-arrow-circle-right"></i></a>
+                                    <a href="#" class="small-box-footer">
+                                    <div class="inner">
+                                    <h3><i class="fa-solid fa-hand-holding-dollar"></i></h3>
+                                    <p>Laporan Pendapatan</p>
+                                    </div></a>
                                     </div>
                                     </div>
 
                                     <div class="col-lg-3 col-6">
-
-                                    <div class="small-box bg-success">
-                                    <div class="inner">
-                                    <h3>#</h3>
-                                    <p>Laporan Pembelian Barang</p>
-                                    </div>
+                                    <div class="small-box bg-info">
                                     <div class="icon">
-                                    <i class="ion ion-stats-bars"></i>
+                                    <i class="ion ion-bag"></i>
                                     </div>
-                                    <a href="#" class="small-box-footer">cek data <i class="fas fa-arrow-circle-right"></i></a>
+                                    <a href="#" class="small-box-footer">
+                                    <div class="inner">
+                                    <h3><i class="fa-solid fa-cubes"></i></h3>
+                                    <p>Laporan Barang</p>
+                                    </div></a>
                                     </div>
                                     </div>
 
                                     <div class="col-lg-3 col-6">
-
-                                    <div class="small-box bg-warning">
-                                    <div class="inner">
-                                    <h3>#</h3>
-                                    <p>laporan Kehadiran</p>
-                                    </div>
+                                    <div class="small-box bg-info">
                                     <div class="icon">
-                                    <i class="ion ion-person-add"></i>
+                                    <i class="ion ion-bag"></i>
                                     </div>
-                                    <a href="#" class="small-box-footer">cek data <i class="fas fa-arrow-circle-right"></i></a>
+                                    <a href="#" class="small-box-footer">
+                                    <div class="inner">
+                                    <h3><i class="fa-solid fa-fingerprint"></i></h3>
+                                    <p>Laporan Absensi</p>
+                                    </div></a>
                                     </div>
                                     </div>
 
                                     <div class="col-lg-3 col-6">
-
-                                    <div class="small-box bg-danger">
-                                    <div class="inner">
-                                    <h3>#</h3>
-                                    <p>Laporan Persediaan Barang</p>
-                                    </div>
+                                    <div class="small-box bg-info">
                                     <div class="icon">
-                                    <i class="ion ion-pie-graph"></i>
+                                    <i class="ion ion-bag"></i>
                                     </div>
-                                    <a href="#" class="small-box-footer">cek data<i class="fas fa-arrow-circle-right"></i></a>
+                                    <a href="#" class="small-box-footer">
+                                    <div class="inner">
+                                    <h3><i class="fa-solid fa-money-bill-trend-up"></i></h3>
+                                    <p>Pemasukan Bulanan</p>
+                                    </div></a>
                                     </div>
                                     </div>
+
+                        
                                     </div>
                                     
                                                                     </div>
